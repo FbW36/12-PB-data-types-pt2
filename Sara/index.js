8356c00ee66d4545c5d5bb6887fbafe618e07d91
Sara <you@example.com>
2020-10-08 15:48:04 +0200

    finished exercise

---
+++ Sara/index.js
@@ -0,0 +1,20 @@
+// 1. Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+let message = 'Hello Beautiful 💕' ;
+console.log('message ==>', message) ; // Hello Beautiful 💕
+
+// 2. Access the 2nd and 6th elements of the string. Print the elements to the console. **Make sure you have selected the right index numbers** 
+console.log('2nd element of message ==>', message[1]) ; // e
+console.log('6nd element of message ==>', message[5]) ; // ''
+// 3. Declare a variable called programmingLanguage and assign the string "JavaScript". Declare a variable called isFun and give it a value of true.    Print "JavaScript is fun: true" to the console, using your declared variables. 🥠 🥠 🥠
+let programmingLanguage = 'JavaScript' ; 
+let isFun = true ;
+
+let strEs2 = `${programmingLanguage} is fun: ${isFun}!` ;
+console.log('print message ==>', strEs2) ; // 
+
+// 4. Print the J and S of JavaScript from the variable programmingLanguage to the console. 
+console.log('print J ==>', programmingLanguage[0]) ; // J
+console.log('print S ==>', programmingLanguage[4]) ; // S
+
+ // 5. Print the length of the string "JavaScript" to the console.
+ console.log('lengt of programmingLanguage ==>', programmingLanguage.length) ;
