caef4f04b4c366b93c3c7215918cc7418060d94a
Chrissi <cmette.online@googlemail.com>
2020-10-08 15:15:58 +0200

    Initial commit

---
+++ christin/index.js
@@ -0,0 +1,13 @@
+// Declare a variable and assign a string as a value.Make sure the string is at least 6 characters long.Print the variable to the console.
+
+let cake = "streuselschnecke";
+
+console.log(cake);
+
+// Access the 2nd and 6th elements of the string.Print the elements to the console.Make sure you have selected the right index numbers
+
+// Declare a variable called programmingLanguage and assign the string "JavaScript".Declare a variable called isFun and give it a value of true.Print "JavaScript is fun: true" to the console, using your declared variables.
+
+// Print the J and S of JavaScript from the variable programmingLanguage to the console.
+
+// Print the length of the string "JavaScript" to the console.
