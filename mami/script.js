5eb7498ae77c27669ee832052c4dc6e3b23617a0
carveler <3heyaberlin@gmail.com>
2020-10-08 16:01:51 +0200

    mami

---
+++ mami/script.js
@@ -0,0 +1,21 @@
+//1. Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+
+let word = "surfing";
+console.log("no1", word);
+
+//2. Access the 2nd and 6th elements of the string. Print the elements to the console. **Make sure you have selected the right index numbers**
+console.log("no2", `${word[1]} ${word[5]}`);
+
+//3. Declare a variable called programmingLanguage and assign the string "JavaScript". Declare a variable called isFun and give it a value of true.    Print "JavaScript is fun: true" to the console, using your declared variables.
+
+let programmingLanguage = "JavaScript";
+let isFun = true;
+
+console.log("no4", `${programmingLanguage} is fun: ${isFun} `);
+
+//4. Print the J and S of JavaScript from the variable programmingLanguage to the console.
+
+console.log("no5", `${programmingLanguage[0]} ${programmingLanguage[4]}`);
+
+//5. Print the length of the string "JavaScript" to the console.
+console.log(programmingLanguage.length);
