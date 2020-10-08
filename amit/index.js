7227b91ee04f0d1c3427c54967c098188e5be64e
Amit833 <amitmajumder833@gmail.com>
2020-10-08 16:09:55 +0200

    amit

---
+++ amit/index.js
@@ -0,0 +1,12 @@
+let city = "Berlin";
+console.log(city);
+console.log("city ==>", city[1], city[5]);
+
+// 3
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(`"${programmingLanguage} is fun:${isFun}"`);
+// 4
+console.log("prog ==>", programmingLanguage[0], programmingLanguage[4]);
+// 5
+console.log("Length ==>", programmingLanguage.length);
