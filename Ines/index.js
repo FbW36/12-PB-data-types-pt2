450b4f3ab3928adcc27e80681512bfc0a0d51010
Ines <inesfcnogueira@gmail.com>
2020-10-08 15:23:14 +0200

    First commit

---
+++ Ines/index.js
@@ -0,0 +1,22 @@
+// 1
+let str = "Halli Hallo";
+console.log("1==>", str);
+
+//2
+console.log("2==>", str.slice(1, 6));
+
+//3
+let programmingLanguage = "JavaScript";
+let isFun = true;
+
+console.log("3==>", programmingLanguage + " is fun:", isFun);
+
+//4
+console.log(
+  "4==>",
+  programmingLanguage.slice(0, 1),
+  programmingLanguage.slice(4, 5)
+);
+
+//5
+console.log("5==>", programmingLanguage.length);
