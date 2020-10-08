c5d9446f67e8fa5ead7e2269d70c8119a6ecfe57
Chrissi <cmette.online@googlemail.com>
2020-10-08 15:28:54 +0200

    Add excercise

--- christin/index.js
+++ christin/index.js
@@ -4,10 +4,20 @@ let cake = "streuselschnecke";
 
 console.log(cake);
 
-// Access the 2nd and 6th elements of the string.Print the elements to the console.Make sure you have selected the right index numbers
+// 2 Access the 2nd and 6th elements of the string.Print the elements to the console.Make sure you have selected the right index numbers
+console.log(cake[1], cake[5]); // t, s
 
-// Declare a variable called programmingLanguage and assign the string "JavaScript".Declare a variable called isFun and give it a value of true.Print "JavaScript is fun: true" to the console, using your declared variables.
+// 3 Declare a variable called programmingLanguage and assign the string "JavaScript".Declare a variable called isFun and give it a value of true.Print "JavaScript is fun: true" to the console, using your declared variables.
 
-// Print the J and S of JavaScript from the variable programmingLanguage to the console.
+let programmingLanguage = "JavaScript";
+let isFun = true;
 
-// Print the length of the string "JavaScript" to the console.
+console.log(`${programmingLanguage} is fun: ${isFun}`);
+
+// 4 Print the J and S of JavaScript from the variable programmingLanguage to the console.
+
+console.log(programmingLanguage[0], programmingLanguage[4]); // J S
+
+// 5 Print the length of the string "JavaScript" to the console.
+
+console.log(programmingLanguage.length); // 10
