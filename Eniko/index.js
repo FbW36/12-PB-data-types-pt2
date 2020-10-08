d9365bd3f4aeaeeccd0afdd559a9924ea5809537
Enikő Veszelka <encsooo@gmail.com>
2020-10-08 14:39:41 +0200

    done (:

---
+++ Eniko/index.js
@@ -0,0 +1,19 @@
+// Exercises to become familiar with data types.
+
+// Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+let var1 = "tea kettle"
+console.log(var1);
+
+// Access the 2nd and 6th elements of the string. Print the elements to the console. Make sure you have selected the right index numbers
+console.log("Access the 2nd and 6th elements of the string: " + var1[2] + var1[6]);  
+
+// Declare a variable called programmingLanguage and assign the string "JavaScript". Declare a variable called isFun and give it a value of true. Print "JavaScript is fun: true" to the console, using your declared variables.
+let programmingLanguage = "JavaScript"
+let isFun = true
+console.log(`${programmingLanguage} is fun: ${isFun}.`);
+
+// Print the J and S of JavaScript from the variable programmingLanguage to the console.
+console.log("Print the J and S of JavaScript from the variable programmingLanguage to the console: " + programmingLanguage[0] + programmingLanguage[4]);
+
+// Print the length of the string "JavaScript" to the console.
+console.log("Print the length of the string JavaScript to the console: " + programmingLanguage.length);
