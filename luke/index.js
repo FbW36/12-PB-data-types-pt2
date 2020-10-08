20a989dbfa4f1befab53a97f253cef88bb3c68dc
Luke Dowling <lpdowling26@googlemail.com>
2020-10-08 14:32:31 +0200

    luke

---
+++ luke/index.js
@@ -0,0 +1,17 @@
+// 1. Declare a variable and assign a string as a value.Make sure the string is at least 6 characters long.Print the variable to the console.
+let job = `programmer`;
+console.log(job);
+
+// 2. Access the 2nd and 6th elements of the string.Print the elements to the console.
+console.log(`${job[1]} ${job[5]}`);
+
+// 3. Declare a variable called programmingLanguage and assign the string "JavaScript".Declare a variable called isFun and give it a value of true.Print "JavaScript is fun: true" to the console, using your declared variables.
+let programmingLanguage = `JavaScript`;
+let isFun = true;
+console.log(`${programmingLanguage} is fun: ${isFun}`);
+
+// 4. Print the J and S of JavaScript from the variable programmingLanguage to the console.
+console.log(`${programmingLanguage[0]} ${programmingLanguage[4]}`);
+
+// 5. Print the length of the string "JavaScript" to the console.
+console.log(programmingLanguage.length);
